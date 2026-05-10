c885077f8bd1d447df1ba49ccb203b52ffe464c1
Amna Yousuf <ayousuf2220@gmail.com>
2026-05-10 14:05:50 -0400

    Simple_Calculator_V1.6.py
    Simple Calculator File:
    Cleaned up the program a bit to make it easier to read. Will implement a class and make it neater next time.
    Added functions to actually work the calculator: displaying digits, decimal places, AC, changing signs, and backspace.
    
    README file:
    Added source I used to help me create the program

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ First project using Python! Will turn it into an executable and swap it with the
 
 Will be able to do basic operations (addition, subtraction, multiplication, division).
 Will also have a history of previous calculations.
+
+Tutorial Used: [text](https://www.youtube.com/watch?v=28tj-IBfGH4) 
--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,6 +1,7 @@
 # Simple Calculator!
 import tkinter
 
+#----------VARIABLES----------
 button_values = [
     ["↻", "AC", "+/-", "←"], #top row
     ["7", "8", "9", "÷"],
@@ -9,35 +10,41 @@ button_values = [
     [".", "0", "=", "+"] #bottom row
 ]
 
-#reference for button values
+#reference
 top_row = button_values[0]
 operation_symbols = ["÷", "x", "-", "+", "=", "."]
 row_count = len(button_values) #5
 column_count = len(button_values[0]) #4
 
-#colours
+#for operations
+A = "0"
+operator = None
+B = None
+
+#for colours
 colour_special_buttons = "#EEA9BF"
 colour_number_buttons = "#FFE0EA"
 colour_text_special = "#FFE8F1"
 colour_text_number = "#000000"
 colour_background = "#FFDBE6"
 
-#window setup
+#----------WINDOW SET UP----------
 window = tkinter.Tk() #creates the window
 window.title("Simple Calculator")
-window.resizable(False, False) #no resizing of the window
+window.resizable(False, False)
 
 frame = tkinter.Frame(window)
 label = tkinter.Label(frame, text = "0", font = ("Average, 45"), bg = colour_background,
-                      foreground = colour_text_number, anchor = "e") #e for east (placement of text)
+                      foreground = colour_text_number, anchor = "e", #e for east (placement of text)
+                      width = column_count)
+label.grid(row = 0, column = 0, columnspan = column_count, sticky = "we") #columnspan spans label across all columns
 
-label.grid(row = 0, column = 0, columnspan = column_count, sticky = "we") #columnspan spans the label across all columns
-                                                                          #we -> west to east spanning of the label
+#----------BUTTONS SET UP----------
 for row in range (row_count):
     for column in range (column_count):
         value = button_values[row][column]
         button = tkinter.Button(frame, text = value, font = ("Average, 30"), width = column_count-1, height = 1,
-                                command = lambda value = value: button.clicked(value))
+                                command = lambda value = value: button_clicked(value))
         
         if value in top_row:
             button.configure(bg = colour_special_buttons, foreground = colour_text_special)
@@ -50,8 +57,92 @@ for row in range (row_count):
 
 frame.pack()
 
+
+#----------MAIN FUNCTIONS----------
 def button_clicked(value):
+    global top_row, operation_symbols, label, A, B, operator
+
+    if value in top_row:
+        special_operations(value)
+    elif value in operation_symbols:
+        operations(value)
+    else:
+        digits(value)
+
+def special_operations(value):
+    global label, A, B, operator
+
+    if value == "↻":
+        history()
+    elif value == "AC":
+        clear_all()
+    elif value == "+/-":
+        changing_sign()
+    else: #backspace
+        backspace()
+
+def operations(value):
+    global label, A, B, operator
+
+    #decimal point
+    if value == ".":
+        if value not in label["text"]:
+            label["text"] += value
+
+    #addition
+
+    #subtraction
+
+    #multiplication
+
+    #division
+
+    #equal
+
+def digits(value):
+    global label
+
+    if label["text"] == "0":
+        label["text"] = value
+    else:
+        label["text"] += value
+
+#----------HELPER FUNCTIONS----------
+def clear_all():
+    global label, A, B, operator
+
+    A = "0"
+    B = None
+    operator = None
+
+    label["text"] = "0"
+
+def changing_sign():
+    global label
+
+    result = float(label["text"]) * -1
+    label["text"] = remove_zero_decimal(result)
+
+def remove_zero_decimal(number):
+    if number % 1 == 0:
+        number = int(number)
+
+    return str(number)
+
+def backspace():
+    global label
+    
+    if label["text"] == "0":
+        return
+    else:
+        label["text"] = label["text"][:-1]
+
+        if label["text"] == "":
+             label["text"] = "0"
+        
+        return label["text"]
+    
+def history():
     pass
 
-window.mainloop() #we want the window to stay open until we close it, so we use mainloop() to keep it running
-
+window.mainloop() #to keep the window open
